785e61bb8edcb18752d18f289ad546f7686d2e69
nazakat Ali <nazakatalimalak289@gmail.com>
2024-03-22 23:22:46 +0500

    created number guessing game

---
+++ main.js
@@ -0,0 +1,17 @@
+import inquirer from "inquirer";
+//computer will genrate rendom number
+//user input  for  guess number
+//compure user  input with computer genrated number show result
+const randomnumber = 34;
+const answers = await inquirer.prompt([{
+        name: "userguessnumber",
+        type: "number",
+        message: "please guess a number:",
+    },
+]);
+if (answers.userguessnumber === randomnumber) {
+    console.log("congragulation you guess right number ");
+}
+else {
+    console.log("you guess wrong number");
+}
